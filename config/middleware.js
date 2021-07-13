baf14d5f6a133bcf57b9d5820411667133634215
Adrian Maseda <amaseda@gmail.com>
2021-07-13 14:06:08 -0400

    Update CORS origins

--- config/middleware.js
+++ config/middleware.js
@@ -6,7 +6,7 @@ module.exports = {
         "http://localhost",
         "http://localhost:1337",
         "http://localhost:3000",
-        "https://cadem-academy-dev.surge.sh/",
+        "https://cadem-academy-dev.surge.sh",
         "https://strapi-j4gf.onrender.com",
       ],
     },
