dac777767531aaa4eb50acf7f1b71e566672e452
Adrian Maseda <amaseda@gmail.com>
2021-07-13 10:11:43 -0400

    Try implementing CORS middleware

---
+++ config/middleware.js
@@ -0,0 +1,12 @@
+module.exports = {
+  settings: {
+    cors: {
+      enabled: true,
+      origin: [
+        "http://localhost",
+        "http://localhost:3000",
+        "https://cadem-academy-dev.surge.sh/",
+      ],
+    },
+  },
+};
